5409779be7fd9513306978c9e422813b634b525d
Kiril Shchasny <kiril3467@yandex.ru>
2017-07-06 20:39:36 +0300

    Add Euclid's algorithm and cover him by NUnit tests

---
+++ GCDSearch.Tests/GCDTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using static GCDSearch.GCD;
+
+namespace GCDSearch.Tests
+{
+    [TestFixture]
+    public class GCDTests
+    {
+        [TestCase(1, 10, ExpectedResult = 1)]
+        [TestCase(10, 5, ExpectedResult = 5)]
+        [TestCase(661, 113, ExpectedResult = 1)]
+        [TestCase(111, 432, ExpectedResult = 3)]
+        [TestCase(24, 24, ExpectedResult = 24)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(5, 0, ExpectedResult = 5)]
+        [TestCase(0, 15, ExpectedResult = 15)]
+        [TestCase(-5, 10, ExpectedResult = 5)]
+        [TestCase(78, 294, 570, 36, ExpectedResult = 6)]
+        [TestCase(10, 20, 30, 40, 50, 60, 70, 80, 90, 100, ExpectedResult = 10)]
+        [TestCase(2, 3, 7, 11, 13, 17, 19, ExpectedResult = 1)]
+        public int AlgorithmOfEuclid_PositiveTests(params int[] nums)
+        {
+            return AlgorithmOfEuclid(nums);
+        }
+
+        [TestCase(1)]
+        [TestCase()]
+        public void AlgorithmOfEuclid_ThrowsArgumentException(params int[] nums)
+        {
+            Assert.Throws<ArgumentException>(() => AlgorithmOfEuclid(nums));
+        }
+
+    }
+}
---
+++ GCDSearch/GCD.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GCDSearch
+{
+    public static class GCD
+    {
+        /// <summary>
+        /// This method allows us to find the greatest common divisor by Euclid's algorithm for set of numbers
+        /// </summary>
+        /// <param name="nums">Set of integer numbers</param>
+        /// <returns>Greatest common divisor</returns>
+        public static int AlgorithmOfEuclid(params int[] nums)
+        {
+            if (nums.Length < 2)
+                throw new ArgumentException();
+
+            int tempGCD = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                tempGCD = EuclidMain(tempGCD, nums[i]);
+            }
+            return tempGCD;
+        }
+
+        /// <summary>
+        /// Classical Euclid's algorithm for 2 numbers
+        /// </summary>
+        /// <param name="a">First num</param>
+        /// <param name="b">Second num</param>
+        /// <returns>Greatest common divisor for 2 numbers</returns>
+        private static int EuclidMain(int a, int b)
+        {
+            return b == 0 ? Math.Abs(a) : EuclidMain(b, a % b);
+        }
+    }
+}

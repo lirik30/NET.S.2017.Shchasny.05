73243c8a125c118062281d02aaf4f300f4ab2112
Kiril Shchasny <kiril3467@yandex.ru>
2017-07-07 10:54:25 +0300

    Add Stein algorithm in GCD class and cover him by NUnit tests

--- GCDSearch.Tests/GCDTests.cs
+++ GCDSearch.Tests/GCDTests.cs
@@ -31,5 +31,30 @@ namespace GCDSearch.Tests
             Assert.Throws<ArgumentException>(() => AlgorithmOfEuclid(nums));
         }
 
+
+        [TestCase(1, 10, ExpectedResult = 1)]
+        [TestCase(10, 5, ExpectedResult = 5)]
+        [TestCase(661, 113, ExpectedResult = 1)]
+        [TestCase(111, 432, ExpectedResult = 3)]
+        [TestCase(24, 24, ExpectedResult = 24)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(5, 0, ExpectedResult = 5)]
+        [TestCase(0, 15, ExpectedResult = 15)]
+        [TestCase(-5, 10, ExpectedResult = 5)]
+        [TestCase(78, 294, 570, 36, ExpectedResult = 6)]
+        [TestCase(10, 20, 30, 40, 50, 60, 70, 80, 90, 100, ExpectedResult = 10)]
+        [TestCase(2, 3, 7, 11, 13, 17, 19, ExpectedResult = 1)]
+        public int AlgorithmOfStein_PositiveTests(params int[] nums)
+        {
+            return AlgorithmOfStein(nums);
+        }
+
+        [TestCase(1)]
+        [TestCase()]
+        public void AlgorithmOfStein_ThrowsArgumentException(params int[] nums)
+        {
+            Assert.Throws<ArgumentException>(() => AlgorithmOfStein(nums));
+        }
+
     }
 }
--- GCDSearch/GCD.cs
+++ GCDSearch/GCD.cs
@@ -2,12 +2,15 @@
 
 namespace GCDSearch
 {
+    /// <summary>
+    /// This class provides opportunities to find the greatest common divisor for several integers 
+    /// </summary>
     public static class GCD
     {
         /// <summary>
-        /// This method allows us to find the greatest common divisor by Euclid's algorithm for set of numbers
+        /// This method allows us to find the greatest common divisor by Euclid's algorithm for set of integers
         /// </summary>
-        /// <param name="nums">Set of integer numbers</param>
+        /// <param name="nums">Set of integers</param>
         /// <returns>Greatest common divisor</returns>
         public static int AlgorithmOfEuclid(params int[] nums)
         {
@@ -22,12 +25,56 @@ namespace GCDSearch
             return tempGCD;
         }
 
+
         /// <summary>
-        /// Classical Euclid's algorithm for 2 numbers
+        /// This method allows us to find the greatest common divisor by Stein algorithm for set of integers
         /// </summary>
-        /// <param name="a">First num</param>
-        /// <param name="b">Second num</param>
-        /// <returns>Greatest common divisor for 2 numbers</returns>
+        /// <param name="nums">Set of integers</param>
+        /// <returns>Greatest common divisor</returns>
+        public static int AlgorithmOfStein(params int[] nums)
+        {
+            if (nums.Length < 2)
+                throw new ArgumentException();
+
+            int tempGCD = Math.Abs(nums[0]);
+            for (int i = 1; i < nums.Length; i++)
+            {
+                tempGCD = SteinMain(tempGCD, Math.Abs(nums[i]));
+            }
+            return tempGCD;
+        }
+
+        /// <summary>
+        /// Stein algorithm for 2 integers
+        /// </summary>
+        /// <param name="a">First integer</param>
+        /// <param name="b">Second integer</param>
+        /// <returns>Greatest common divisor for 2 integers</returns>
+        private static int SteinMain(int a, int b)
+        {
+            if (a == b) return a;
+            if (a == 0) return b;
+            if (b == 0) return a;
+
+            if ((a & 1) != 1)
+            {
+                if ((b & 1) == 1)
+                    return SteinMain(a >> 1, b);
+                return SteinMain(a >> 1, b >> 1) << 1;
+            }
+
+            if ((b & 1) != 1)
+                return SteinMain(a, b >> 1);
+
+            return a > b ? SteinMain((a - b) >> 1, b) : SteinMain((b - a) >> 1, a);
+        }
+
+        /// <summary>
+        /// Classical Euclid's algorithm for 2 integers
+        /// </summary>
+        /// <param name="a">First integer</param>
+        /// <param name="b">Second integer</param>
+        /// <returns>Greatest common divisor for 2 integers</returns>
         private static int EuclidMain(int a, int b)
         {
             return b == 0 ? Math.Abs(a) : EuclidMain(b, a % b);

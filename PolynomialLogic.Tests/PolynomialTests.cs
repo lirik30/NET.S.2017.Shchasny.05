b7572c9b807b59421413602618fc3f03b60ccfd7
Kiril Shchasny <kiril3467@yandex.ru>
2017-07-09 12:53:57 +0300

    Add Polynomial class and cover him by several tests

---
+++ PolynomialLogic.Tests/PolynomialTests.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using static PolynomialLogic.Polynomial;
+using NUnit.Framework;
+
+namespace PolynomialLogic.Tests
+{
+    [TestFixture]
+    public class PolynomialTests
+    {
+        #region AddTest
+        private static IEnumerable<TestCaseData> AddTestData_Positive
+        {
+            get
+            {
+                yield return new TestCaseData(
+                        new Polynomial(1.0, 0, 5.5, -6.45, 1.25),
+                        new Polynomial(0.0, 5.0, -4.0, 3.5)).
+                    Returns("1+5x^1+1,5x^2-2,95x^3+1,25x^4");
+
+                yield return new TestCaseData(
+                        new Polynomial(0.0, 5.0, -4.0, 3.5),
+                        new Polynomial(1.0, 0, 5.5, -6.45, 1.25)).
+                    Returns("1+5x^1+1,5x^2-2,95x^3+1,25x^4");
+            }
+        }
+
+        [Test, TestCaseSource(nameof(AddTestData_Positive))]
+        public string Add_Positive(Polynomial a, Polynomial b)
+        {
+            return Add(a, b).ToString();
+        }
+
+        [TestCase(new double[] {1.0, 0, 5.5, -6.45, 1.25})]
+        public void Add_ThrowsArgumentNullException(double[] a)
+        {
+            Assert.Throws<ArgumentNullException>(() => Add(new Polynomial(a), null));
+            Assert.Throws<ArgumentNullException>(() => Add(null, new Polynomial(a)));
+            Assert.Throws<ArgumentNullException>(() => Add(null, null));
+        }
+        #endregion
+
+        #region SubtractTests
+        private static IEnumerable<TestCaseData> SubtractTestData_Positive
+        {
+            get
+            {
+                yield return new TestCaseData(
+                        new Polynomial(1.0, 0, 5.5, -6.45, 1.25),
+                        new Polynomial(0.0, 5.0, -4.0, 3.5)).
+                    Returns("1-5x^1+9,5x^2-9,95x^3+1,25x^4");
+            }
+        }
+
+        [Test, TestCaseSource(nameof(SubtractTestData_Positive))]
+        public string Subtract_Positive(Polynomial a, Polynomial b)
+        {
+            return Subtract(a, b).ToString();
+        }
+
+        [TestCase(new double[] { 1.0, 0, 5.5, -6.45, 1.25 })]
+        public void Subtract_ThrowsArgumentNullException(double[] a)
+        {
+            Assert.Throws<ArgumentNullException>(() => Subtract(new Polynomial(a), null));
+            Assert.Throws<ArgumentNullException>(() => Subtract(null, new Polynomial(a)));
+            Assert.Throws<ArgumentNullException>(() => Subtract(null, null));
+        }
+        #endregion
+
+        #region MultiplyTests
+        private static IEnumerable<TestCaseData> MultiplyTestData_Positive
+        {
+            get
+            {
+                yield return new TestCaseData(
+                        new Polynomial(1.0, 0, 5.5, -6.45, 1.25),
+                        new Polynomial(0.0, 5.0, -4.0, 3.5)).
+                    Returns("0+5x^1-4x^2+31x^3-54,25x^4+51,3x^5-27,575x^6+4,375x^7");
+
+                yield return new TestCaseData(
+                        new Polynomial(0.0, 5.0, -4.0, 3.5),
+                        new Polynomial(1.0, 0, 5.5, -6.45, 1.25)).
+                    Returns("0+5x^1-4x^2+31x^3-54,25x^4+51,3x^5-27,575x^6+4,375x^7");
+            }
+        }
+        
+        [Test, TestCaseSource(nameof(MultiplyTestData_Positive))]
+        public string Multiply_Positive(Polynomial a, Polynomial b)
+        {
+            return Multiply(a, b).ToString();
+        }
+
+        [TestCase(new double[] { 1.0, 0, 5.5, -6.45, 1.25 })]
+        public void Multiply_ThrowsArgumentNullException(double[] a)
+        {
+            Assert.Throws<ArgumentNullException>(() => Multiply(new Polynomial(a), null));
+            Assert.Throws<ArgumentNullException>(() => Multiply(null, new Polynomial(a)));
+            Assert.Throws<ArgumentNullException>(() => Multiply(null, null));
+        }
+        #endregion
+
+        #region OppositeTests
+        private static IEnumerable<TestCaseData> OppositeTestData_Positive
+        {
+            get
+            {
+                yield return new TestCaseData(
+                        new Polynomial(1.0, 0, 5.5, -6.45, 1.25)).
+                    Returns("-1-5,5x^2+6,45x^3-1,25x^4");
+            }
+        }
+
+        [Test, TestCaseSource(nameof(OppositeTestData_Positive))]
+        public string Opposite_Positive(Polynomial a)
+        {
+            return (-a).ToString();
+        }
+
+        [TestCase(new double[] { 1.0, 0, 5.5, -6.45, 1.25 })]
+        public void Opposite_ThrowsArgumentNullException(double[] a)
+        {
+            Assert.Throws<ArgumentNullException>(() => Opposite(null));
+        }
+
+
+        #endregion
+
+        #region Equals/NotEqualsTests
+        private static IEnumerable<TestCaseData> EqualsTestData_Positive
+        {
+            get
+            {
+                yield return new TestCaseData(
+                        new Polynomial(1.0, 0, 5.5, -6.45, 1.25),
+                        new Polynomial(1.0, 0, 5.5, -6.45, 1.25)).
+                    Returns(true);
+
+                yield return new TestCaseData(
+                        new Polynomial(4.0, 0, 5.5, -6.45, 1.25),
+                        new Polynomial(1.0, 0, 5.5, -6.45, 1.25)).
+                    Returns(false);
+
+                yield return new TestCaseData(
+                        null,
+                        new Polynomial(1.0, 0, 5.5, -6.45, 1.25)).
+                    Returns(false);
+
+                yield return new TestCaseData(
+                        new Polynomial(1.0, 0, 5.5, -6.45, 1.25),
+                        null).
+                    Returns(false);
+
+                yield return new TestCaseData(
+                        null,
+                        null).
+                    Returns(true);
+            }
+        }
+
+        private static IEnumerable<TestCaseData> NotEqualsTestData_Positive
+        {
+            get
+            {
+                yield return new TestCaseData(
+                        new Polynomial(1.0, 0, 5.5, -6.45, 1.25),
+                        new Polynomial(1.0, 0, 5.5, -6.45, 1.25)).
+                    Returns(false);
+
+                yield return new TestCaseData(
+                        new Polynomial(4.0, 0, 5.5, -6.45, 1.25),
+                        new Polynomial(1.0, 0, 5.5, -6.45, 1.25)).
+                    Returns(true);
+
+                yield return new TestCaseData(
+                        null,
+                        new Polynomial(1.0, 0, 5.5, -6.45, 1.25)).
+                    Returns(true);
+
+                yield return new TestCaseData(
+                        new Polynomial(1.0, 0, 5.5, -6.45, 1.25),
+                        null).
+                    Returns(true);
+
+                yield return new TestCaseData(
+                        null,
+                        null).
+                    Returns(false);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(EqualsTestData_Positive))]
+        public bool Equals_Positive(Polynomial a, Polynomial b)
+        {
+            return a == b;
+        }
+
+        [Test, TestCaseSource(nameof(NotEqualsTestData_Positive))]
+        public bool NotEquals_Positive(Polynomial a, Polynomial b)
+        {
+            return a != b;
+        }
+        #endregion
+    }
+}
---
+++ PolynomialLogic/Polynomial.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Linq;
+
+namespace PolynomialLogic
+{
+    public class Polynomial
+    {
+        //TODO: add possibility to do something if size = 0 or if one of params in operator overload is null
+
+        private double[] coeffs;
+        public int Size => coeffs.Length;
+
+        #region ctors
+        public Polynomial(params double[] coeffs)
+        {
+            if (coeffs == null)
+                throw new ArgumentNullException();
+
+            this.coeffs = new double[coeffs.Length];
+            for (int i = 0; i < coeffs.Length; i++)
+                this[i] = coeffs[i];
+        }
+
+        public Polynomial(Polynomial copy) : this(copy.coeffs) { }
+        public Polynomial(int size) : this(new double[size]) { }
+
+        #endregion
+
+        #region Operator overload
+
+        public static Polynomial operator +(Polynomial a, Polynomial b)
+        {
+            CheckArguments(a, b);
+
+            var result = a.Size > b.Size ? new Polynomial(a.Size) : new Polynomial(b.Size);
+            for (int i = 0; i < result.Size; i++)
+            {
+                if (i >= a.Size) result[i] = b[i];
+                else if (i >= b.Size) result[i] = a[i];
+                else result[i] = a[i] + b[i];
+            }
+
+            return result;
+        }
+
+        public static Polynomial operator -(Polynomial a)
+        {
+            CheckArguments(a);
+            var result = new Polynomial(a);
+            for (int i = 0; i < result.Size; i++)
+                result[i] = -result[i];
+            return result;
+        }
+
+        public static Polynomial operator -(Polynomial a, Polynomial b) => a + (-b);
+
+        public static Polynomial operator *(Polynomial a, Polynomial b)
+        {
+            CheckArguments(a, b);
+            if(a.Size == 0 || b.Size == 0)
+                throw new ArgumentOutOfRangeException();
+
+            var result = new Polynomial((a.Size - 1) + (b.Size - 1) + 1);
+
+            for (int i = 0; i < a.Size; i++)
+                for (int j = 0; j < b.Size; j++)
+                    result[i + j] += a[i] * b[j];
+
+            return result;
+        }
+
+        public static bool operator ==(Polynomial a, Polynomial b)
+        {
+            if (Equals(a, null) && Equals(b, null)) return true;
+            if (!Equals(a, null) && !Equals(b, null)) return a.Equals(b);
+            return false;
+        }
+
+        public static bool operator !=(Polynomial a, Polynomial b)
+        {
+            if (Equals(a, null) && Equals(b, null)) return false;
+            if (!Equals(a, null) && !Equals(b, null)) return !a.Equals(b);
+            return true;
+        }
+
+        //public static bool operator == (Polynomial a, Polynomial b) => !Equals(b, null) ? !Equals(a, null) && a.Equals(b) : Equals(a, null);
+
+        //public static bool operator != (Polynomial a, Polynomial b) => !Equals(b, null) ? !Equals(a, null) && !a.Equals(b) : !Equals(a, null);
+
+        #endregion
+
+        #region Static methods
+
+        public static Polynomial Add(Polynomial a, Polynomial b)      => a + b;
+
+        public static Polynomial Subtract(Polynomial a, Polynomial b) => a - b;
+
+        public static Polynomial Multiply(Polynomial a, Polynomial b) => a * b;
+
+        public static Polynomial Opposite(Polynomial a)               => -a;
+
+        #endregion
+
+        #region Object methods
+
+        public override bool Equals(object obj)
+        {
+            var temp = obj as Polynomial;
+            if (temp?.Size != Size)
+                return false;
+
+            for (int i = 0; i < this.Size; i++)
+            {
+                if (this.coeffs[i] != temp.coeffs[i])
+                    return false;
+            }
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return coeffs.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            string result = $"{coeffs[0]}";
+            for (int i = 1; i < coeffs.Length; i++)
+            {
+                if (Math.Abs(coeffs[i] - 0) < double.Epsilon) continue;
+                result += coeffs[i] > 0 ? $"+{coeffs[i]}x^{i}" : $"{coeffs[i]}x^{i}";
+            }
+            return result;
+        }
+        #endregion
+
+        public double Calculate(double num)
+        {
+            double result = coeffs[0];
+            for (int i = 1; i < Size; i++)
+            {
+                result += coeffs[i] * num;
+                num *= num;
+            }
+            return result;
+        }
+
+        public double this[int index]
+        {
+            get => index > Size - 1 ? throw new ArgumentOutOfRangeException() : coeffs[index];
+            set
+            {
+                if (index > Size - 1) throw new ArgumentOutOfRangeException();
+                coeffs[index] = value;
+            }
+        }
+
+        private static void CheckArguments(params object[] args)
+        {
+            if (args.Any(arg => arg == null))
+            {
+                throw new ArgumentNullException();
+            }
+        }
+    }
+}

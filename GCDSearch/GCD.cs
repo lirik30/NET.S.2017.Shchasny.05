ab53256e6c8e777898f83c5876a2cba0aea0cf9a
Kiril Shchasny <kiril3467@yandex.ru>
2017-07-20 17:05:30 +0300

    GCD class refactoring

--- GCDSearch/GCD.cs
+++ GCDSearch/GCD.cs
@@ -8,6 +8,53 @@ namespace GCDSearch
     /// </summary>
     public static class GCD
     {
+        /// <summary>
+        /// Delegate provides us with way to use Adapter method with different algorithms
+        /// </summary>
+        /// <param name="lhs">First integer</param>
+        /// <param name="rhs">Second integer</param>
+        /// <returns></returns>
+        private delegate int Algorithm(int lhs, int rhs);
+
+
+        /// <summary>
+        /// This method allows us to find GCD of 2 integers with any algorithm, described by delegate
+        /// </summary>
+        /// <param name="algo">Delegate that describe used algorithm</param>
+        /// <param name="lhs">First integer</param>
+        /// <param name="rhs">Second integer</param>
+        /// <param name="time">Method execution time</param>
+        /// <returns></returns>
+        private static int Adapter(Algorithm algo, int lhs, int rhs, out long time)
+        {
+            var start = Stopwatch.StartNew();
+            int result = algo(Math.Abs(lhs), Math.Abs(rhs));
+            time = start.ElapsedMilliseconds;
+            return result;
+        }
+
+
+        /// <summary>
+        /// This method allows us to find GCD of set of integers with any algorithm, described by delegate
+        /// </summary>
+        /// <param name="algo">Delegate that describe used algorithm</param>
+        /// <param name="time">Method execution time</param>
+        /// <param name="nums">Set of integers</param>
+        /// <returns></returns>
+        private static int Adapter(Algorithm algo, out long time, params int[] nums)
+        {
+            if (nums.Length < 2)
+                throw new ArgumentException();
+
+            var start = Stopwatch.StartNew();
+
+            int tempGCD = Math.Abs(nums[0]);
+            for (int i = 1; i < nums.Length; i++)
+                tempGCD = algo(tempGCD, Math.Abs(nums[i]));
+            time = start.ElapsedMilliseconds;
+            return tempGCD;
+        }
+
 
         /// <summary>
         /// This method allows us to find the greatest common divisor by Euclid's algorithm for 2 integers
@@ -17,13 +64,8 @@ namespace GCDSearch
         /// <param name="first">First integer</param>
         /// <param name="second">Second integer</param>
         /// <returns>Greatest common divisor</returns>
-        public static int AlgorithmOfEuclid(out long time, int first, int second)
-        {
-            var start = Stopwatch.StartNew();
-            int result = EuclidMain(first, second);
-            time = start.ElapsedMilliseconds;
-            return result;
-        }
+        public static int AlgorithmOfEuclid(out long time, int first, int second) 
+            => Adapter(EuclidMain, first, second, out time);
 
 
         /// <summary>
@@ -33,20 +75,7 @@ namespace GCDSearch
         /// <param name="time">Method execution time</param>
         /// <param name="nums">Set of integers</param>
         /// <returns>Greatest common divisor</returns>
-        public static int AlgorithmOfEuclid(out long time, params int[] nums)
-        {
-            var start = Stopwatch.StartNew();
-            if (nums.Length < 2)
-                throw new ArgumentException();
-
-            int tempGCD = nums[0];
-            for (int i = 1; i < nums.Length; i++)
-            {
-                tempGCD = EuclidMain(tempGCD, nums[i]);
-            }
-            time = start.ElapsedMilliseconds; 
-            return tempGCD;
-        }
+        public static int AlgorithmOfEuclid(out long time, params int[] nums) => Adapter(EuclidMain, out time, nums);
 
 
         /// <summary>
@@ -57,13 +86,8 @@ namespace GCDSearch
         /// <param name="first">First integer</param>
         /// <param name="second">Second integer</param>
         /// <returns>Greatest common divisor</returns>
-        public static int AlgorithmOfStein(out long time, int first, int second)
-        {
-            var start = Stopwatch.StartNew();
-            int result = SteinMain(first, second);
-            time = start.ElapsedMilliseconds;
-            return result;
-        }
+        public static int AlgorithmOfStein(out long time, int first, int second) 
+            => Adapter(SteinMain, first, second, out time);
 
 
         /// <summary>
@@ -73,20 +97,7 @@ namespace GCDSearch
         /// <param name="time">Method execution time</param>
         /// <param name="nums">Set of integers</param>
         /// <returns>Greatest common divisor</returns>
-        public static int AlgorithmOfStein(out long time, params int[] nums)
-        {
-            var start = Stopwatch.StartNew();
-            if (nums.Length < 2)
-                throw new ArgumentException();
-
-            int tempGCD = Math.Abs(nums[0]);
-            for (int i = 1; i < nums.Length; i++)
-            {
-                tempGCD = SteinMain(tempGCD, Math.Abs(nums[i]));
-            }
-            time = start.ElapsedMilliseconds;
-            return tempGCD;
-        }
+        public static int AlgorithmOfStein(out long time, params int[] nums) => Adapter(SteinMain, out time, nums);
 
 
         /// <summary>
@@ -121,9 +132,6 @@ namespace GCDSearch
         /// <param name="a">First integer</param>
         /// <param name="b">Second integer</param>
         /// <returns>Greatest common divisor for 2 integers</returns>
-        private static int EuclidMain(int a, int b)
-        {
-            return b == 0 ? Math.Abs(a) : EuclidMain(b, a % b);
-        }
+        private static int EuclidMain(int a, int b) => b == 0 ? a : EuclidMain(b, a % b);
     }
 }
